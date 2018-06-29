5360bdef6837df2d1c48c507910af9e66b1c64d8
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-29 17:48:01 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,6 +10,8 @@ class SchoolClassesController < ApplicationController
 
   def create
     @school_class = SchoolClass.create(params.require(:school_class).permit(:title, :room_number))
+
+    redirect_to school_class_path(@school_class)
   end
 
   def update
