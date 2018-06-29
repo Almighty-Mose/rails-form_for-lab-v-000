d070d8765fdd0d450cba2bed3cbeb9bd820edfe9
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-29 17:27:57 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,19 @@
 class SchoolClassesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
 end
