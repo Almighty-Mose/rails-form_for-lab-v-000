e1a2cfd34f745baa53a257b9b7ee4940a4771aa0
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-29 18:20:11 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,7 +17,7 @@ class StudentsController < ApplicationController
 
   def update
     find_student
-    @student = Student.update(pass_params)
+    @student = Student.update(params.require(:student).permit(:first_name, :last_name))
 
     redirect_to student_path(@student)
   end
