33e94b941ba2ad450786ee07c248836df0eadb8e
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-29 18:24:08 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,7 +18,7 @@ class StudentsController < ApplicationController
 
   def update
     find_student
-    binding.pry
+    #binding.pry
     @student = Student.update(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to student_path(@student)
   end
